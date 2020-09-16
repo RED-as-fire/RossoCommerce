c5b7d64659fc4fde99f1c42a8a1d3ded8ce72ca0
Federico Picone <federico.picone.develop@gmail.com>
2020-09-16 12:36:16 +0200

    registrazione ts e html completati

--- src/app/features/autenticazione/registrazione/registrazione.component.ts
+++ src/app/features/autenticazione/registrazione/registrazione.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {AutenticazioneFacadeService} from '../services/autenticazione-facade.service';
+import {Store} from '@ngrx/store';
+import {selectUserState} from '../../../redux';
 
 @Component({
   selector: 'app-registrazione',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrazioneComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  get emailControl(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+  }
+  get passwordControl(): FormControl {
+    return this.registerForm.get('password') as FormControl;
+  }
+  get nomeControl(): FormControl {
+    return this.registerForm.get('nome') as FormControl;
+  }
+  get cognomeControl(): FormControl {
+    return this.registerForm.get('cognome') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder, private service: AutenticazioneFacadeService, private store: Store) {
+    this.registerForm = this.fb.group({
+      email: [''],
+      password: [''],
+      nome: [''],
+      cognome: ['']
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  registrazioneUtente() {
+    console.log(this.emailControl.value, this.passwordControl.value, this.cognomeControl.value, this.nomeControl.value);
+    this.service.registrazioneFatta(this.emailControl.value, this.passwordControl.value, this.nomeControl.value, this.cognomeControl.value);
+  }
 }
